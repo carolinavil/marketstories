d87013e1819a3cb7e1ec6faaddc63de2564421cb
carolinavil <acarolinavilel4@gmail.com>
2024-05-24 15:09:49 -0300

    22

--- src/app/parts/index/index.routing.ts
+++ src/app/parts/index/index.routing.ts
@@ -10,8 +10,8 @@ const login = () => import('./login/login.module').then(res => res.LoginModule);
 const painel = () => import('./painel/login.module').then(res => res.PainelModule);
 const pagamento = () => import('./pagamento/pagamento.module').then(res => res.PagamentoModule);
 const routes: Routes = [
-  // { path: '', component: IndexComponent },
-  { path: '', component: IndexTesteComponent },
+  { path: '', component: IndexComponent },
+  { path: 'teste', component: IndexTesteComponent },
   { path: 'pagamento', loadChildren: pagamento },
   { path: 'login', loadChildren: login },
   { path: 'painel/:id', loadChildren: painel },
